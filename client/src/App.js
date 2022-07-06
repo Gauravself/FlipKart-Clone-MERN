7876e4f8a4a1226a57bf36a6783d11ec6fa1a093
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-07-06 17:37:39 +0530

    Buidling Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,60 @@
 import "./App.css";
-import Header from "./components/header/Header";
+import { AppBar, Toolbar, Typography, Box, styled } from "@mui/material";
+import Search from "./components/header/Search";
+import CustomButtons from "./components/header/CustomButtons";
+
+const StyledHeader = styled(AppBar)`
+  background: #2874f0;
+  height: 55px;
+`;
+
+const Component = styled(Box)`
+  margin-left: 12%;
+  line-height: 0;
+`;
+
+const Subheading = styled(Typography)`
+  font-size: 10px;
+  font-style: italic;
+`;
+
+//Put HTML element in string
+const Plusimage = styled("img")({
+  width: 10,
+  height: 10,
+  marginLeft: 4,
+});
+
+const CustomButtonWrapper = styled(Box)`
+  margin: 0 5% 0 auto;
+`;
 
 function App() {
+  const logoURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+  const subURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <StyledHeader>
+      <Toolbar style={{ minHeight: 55 }}>
+        <Component>
+          <img src={logoURL} alt="logo" style={{ width: 75 }} />
+          <Box style={{ display: "flex" }}>
+            <Subheading>
+              Explore&nbsp;
+              <Box component="span" style={{ color: "yellow" }}>
+                Plus
+              </Box>
+            </Subheading>
+            <Plusimage src={subURL} alt="sub-logo" />
+          </Box>
+        </Component>
+        <Search />
+        <CustomButtonWrapper>
+          <CustomButtons />
+        </CustomButtonWrapper>
+      </Toolbar>
+    </StyledHeader>
   );
 }
 
---
+++ client/src/components/header/Search.jsx
@@ -0,0 +1,35 @@
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, InputBase, styled } from "@mui/material";
+
+const SearchContainer = styled(Box)`
+  background: white;
+  width: 38%;
+  border-radius: 2px;
+  margin-left: 10px;
+  display: flex;
+`;
+const InputSearchBase = styled(InputBase)`
+  padding-left: 20px;
+  width: 100%;
+  font-size: unset;
+  font-family: Arial, sans-serif;
+  letter-spacing: -0.2px;
+`;
+
+const SearchIconWrapper = styled(Box)`
+  color: blue;
+  padding: 5px;
+`;
+
+const Search = () => {
+  return (
+    <SearchContainer>
+      <InputSearchBase placeholder="Search for Products, brands and more" />
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+    </SearchContainer>
+  );
+};
+
+export default Search;
