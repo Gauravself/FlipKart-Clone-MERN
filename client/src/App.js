fe1b72fb475b066a6d008f39c5ca7094d534c025
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-07-08 12:24:37 +0530

    Category Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,16 @@
-import "./App.css";
-import { AppBar, Toolbar, Typography, Box, styled } from "@mui/material";
-import Search from "./components/header/Search";
-import CustomButtons from "./components/header/CustomButtons";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
-const StyledHeader = styled(AppBar)`
-  background: #2874f0;
-  height: 55px;
-`;
-
-const Component = styled(Box)`
-  margin-left: 12%;
-  line-height: 0;
-`;
-
-const Subheading = styled(Typography)`
-  font-size: 10px;
-  font-style: italic;
-`;
-
-//Put HTML element in string
-const Plusimage = styled("img")({
-  width: 10,
-  height: 10,
-  marginLeft: 4,
-});
-
-const CustomButtonWrapper = styled(Box)`
-  margin: 0 5% 0 auto;
-`;
+import { Box } from "@mui/material";
 
 function App() {
-  const logoURL =
-    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
-  const subURL =
-    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
   return (
-    <StyledHeader>
-      <Toolbar style={{ minHeight: 55 }}>
-        <Component>
-          <img src={logoURL} alt="logo" style={{ width: 75 }} />
-          <Box style={{ display: "flex" }}>
-            <Subheading>
-              Explore&nbsp;
-              <Box component="span" style={{ color: "yellow" }}>
-                Plus
-              </Box>
-            </Subheading>
-            <Plusimage src={subURL} alt="sub-logo" />
-          </Box>
-        </Component>
-        <Search />
-        <CustomButtonWrapper>
-          <CustomButtons />
-        </CustomButtonWrapper>
-      </Toolbar>
-    </StyledHeader>
+    <>
+      <Header />
+      <Box style={{ marginTop: 54 }}>
+        <Home />
+      </Box>
+    </>
   );
 }
 
