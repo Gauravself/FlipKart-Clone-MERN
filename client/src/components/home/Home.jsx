5f8af6dc7f96d4876db407e78e9d984366102cff
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-07-09 09:54:07 +0530

    Backend

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,19 @@
 import Banner from "./Banner";
 import NavBar from "./NavBar";
+import { Box, styled } from "@mui/material";
+
+const Component = styled(Box)`
+  padding: 10px 10px;
+  background: #f2f2f2;
+`;
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <Banner />
+      <Component>
+        <Banner />
+      </Component>
     </>
   );
 };
